c226f37e2e0c51f31677daeb8e65e1d674e7e3da
kunal bhanuse <kunalbhanuse07@gmail.com>
2026-05-10 23:55:20 +0530

    authMiddleware,me endpoints done

---
+++ backend/src/module/auth/auth.middleware.js
@@ -0,0 +1,32 @@
+import ApiError from "../../common/utility/ApiError.js";
+import { verifyAccessToken } from "../../common/utility/jwt.utility.js";
+import User from "../../module/auth/auth.model.js";
+
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const header = req.headers.authorization;
+
+    if (!header || !header.startsWith("Bearer ")) {
+      return next(ApiError.unauthorized("Unauthorized"));
+    }
+
+    const token = header.split(" ")[1];
+
+    if (!token) {
+      return next(ApiError.unauthorized("Unauthorized"));
+    }
+
+    const decoded = verifyAccessToken(token);
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return next(ApiError.unauthorized("Unauthorized"));
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return next(ApiError.unauthorized("Invalid or expired token"));
+  }
+};
--- backend/src/module/auth/auth.route.js
+++ backend/src/module/auth/auth.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import * as authController from "./auth.controller.js";
+import { authMiddleware } from "../auth/auth.middleware.js";
 
 const authRouter = Router();
 authRouter.post("/login", authController.login);
 authRouter.post("/register", authController.register);
 authRouter.get("/verify-email", authController.verifyEmail);
+authRouter.get("/me", authMiddleware, authController.me);
 
 export default authRouter;
